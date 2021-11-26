6be06632db04a8926cf0e0b13ff95de09c99f469
Fehmi Nechba <nechba.fehmi@gmail.com>
2021-11-26 15:36:34 +0100

    todoapp project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <todoApp msg="Welcome to Your Vue.js App"> </todoApp>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todoApp from './components/todoApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    todoApp,
   }
 }
 </script>
---
+++ src/components/todoApp.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container mt-5">
+    <h1 class="mt-5">My vue to do App </h1>
+
+    <div class="d-flex">
+
+      <input type="text" class="form-control mr-2">
+      <button class="btn btn-warning">submit</button>
+    </div>
+
+<div class="container mt-5">
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Status</th>
+      <th scope="col">#</th>
+      <th scope="col">#</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td><span class="fa fa-pen"></span></td>
+      <td><span class="fa fa-trash"></span></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td><span class="fa fa-pen"></span></td>
+      <td><span class="fa fa-trash"></span></td>
+    </tr>
+  
+  </tbody>
+</table>
+
+
+
+
+
+
+
+</div>
+      
+
+
+
+   
+   
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
